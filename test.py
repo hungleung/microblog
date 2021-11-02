66c808efd70e70333b958d05655beda80f2be948
hungleung <hungleung.lee@me.com>
2021-11-02 10:47:50 +0800

    before deploy

---
+++ test.py
@@ -0,0 +1,35 @@
+import requests, json
+
+# Add your subscription key and endpoint
+subscription_key = "de236c5c419640de89bee4836e089f77"
+endpoint = "https://api.cognitive.microsofttranslator.com"
+
+# Add your location, also known as region. The default is global.
+# This is required if using a Cognitive Services resource.
+location = "eastasia"
+
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['de', 'zh-cn']
+}
+constructed_url = endpoint + path
+
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json'
+}
+
+# You can pass more than one object in body.
+body = [{
+    'text': 'Hello World!'
+}]
+
+request = requests.post(constructed_url, params=params, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
